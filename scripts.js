2a0154403943242706a3a045b6ef65e6d8cf2376
agtoVrgs <38594995+agtoVrgs@users.noreply.github.com>
2024-04-18 12:13:56 -0500

    Add files via upload
    Initial commit of website files

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Add your JavaScript code here
+document.addEventListener('DOMContentLoaded', function() {
+    // DOM is fully loaded, but maybe waiting on images & stylesheets
+    console.log("Welcome to my portfolio site!");
+});
