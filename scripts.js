893cc0c0ad15436bb3cdef45a048cb4455857cea
agtoVrgs <38594995+agtoVrgs@users.noreply.github.com>
2024-04-24 21:37:43 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
+
 function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "250px";
-    }
+    // Toggle a class that controls the width
+    sidebar.classList.toggle("active");
 }
